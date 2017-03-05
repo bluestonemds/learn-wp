8ca667cf61c68e21a33738b2adc427fdf30bc250
bluestonemds <xiaofeng1836@gmail.com>
2017-03-05 15:23:18 +0800

    display posts.

--- wp-content/themes/learnproject/functions.php
+++ wp-content/themes/learnproject/functions.php
@@ -2,9 +2,9 @@
 
 
 function learningWordPress_resources(){
-	wp_enqueue_style("style",get_stylesheet_uri());
     wp_enqueue_style("grids_responsive",get_template_directory_uri().'/assets/css/grids-responsive-min.css');
     wp_enqueue_style("pure",get_template_directory_uri().'/assets/css/pure-min.css');
+    wp_enqueue_style("style",get_stylesheet_uri());
 }
 
 add_action("wp_enqueue_scripts",'learningWordPress_resources');
@@ -13,3 +13,4 @@ function add_menuclass($ulclass) {
     return preg_replace('/<a /', '<a class="pure-menu-link"', $ulclass);
 }
 add_filter('wp_nav_menu','add_menuclass');
+
--- wp-content/themes/learnproject/index.php
+++ wp-content/themes/learnproject/index.php
@@ -18,12 +18,11 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
-        <?php if (have_posts()) :
-
+        <?php
+        if (have_posts()) :
             while (have_posts()) : the_post(); ?>
                 <h2><?php the_title(); ?></h2>
                 <?php the_content();
-            
             endwhile;
         endif;
         ?>

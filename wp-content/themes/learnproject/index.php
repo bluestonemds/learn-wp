07d2d1d495c6d38068162debec6065f617879ae0
bluestonemds <xiaofeng1836@gmail.com>
2017-03-05 20:20:33 +0800

    display posts.

--- wp-content/themes/learnproject/index.php
+++ wp-content/themes/learnproject/index.php
@@ -13,21 +13,15 @@
  * @subpackage Twenty_Sixteen
  * @since Twenty Sixteen 1.0
  */
-
 get_header(); ?>
-
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+<div class="container">
         <?php
         if (have_posts()) :
             while (have_posts()) : the_post(); ?>
                 <h2><?php the_title(); ?></h2>
-                <?php the_content();
-            endwhile;
+            <?php endwhile;
         endif;
         ?>
-
-    </main><!-- .site-main -->
-</div><!-- .content-area -->
+</div>
 
 <?php get_footer(); ?>
